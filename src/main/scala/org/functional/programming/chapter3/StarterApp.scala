ed68f1fa91bc5f4a27cd69febbf098f5c408ed15
adnanrahin <adnan.rahin13@gmail.com>
2022-08-28 03:45:23 -0400

    update

--- src/main/scala/org/functional/programming/chapter3/StarterApp.scala
+++ src/main/scala/org/functional/programming/chapter3/StarterApp.scala
@@ -2,4 +2,15 @@ package org.functional.programming.chapter3
 
 object StarterApp {
 
+  def main(args: Array[String]): Unit = {
+
+    val greetStrings = new Array[String](3)
+    greetStrings(0) = "Hello"
+    greetStrings(1) = ", "
+    greetStrings(2) = "world!\n"
+    for (i <- 0 to 2)
+      print(greetStrings(i))
+
+  }
+
 }
