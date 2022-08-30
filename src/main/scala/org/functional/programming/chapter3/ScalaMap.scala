454894a0479bca773577c1fede65fc7dcfadf728
adnanrahin <adnan.rahin13@gmail.com>
2022-08-30 01:58:52 -0400

    update

--- src/main/scala/org/functional/programming/chapter3/ScalaMap.scala
+++ src/main/scala/org/functional/programming/chapter3/ScalaMap.scala
@@ -4,7 +4,10 @@ package org.functional.programming.chapter3
 object ScalaMap {
 
   def main(args: Array[String]): Unit = {
-
+    val romanNumeral = Map(
+      1 -> "I", 2 -> "II", 3 -> "III", 4 -> "IV", 5 -> "V"
+    )
+    println(romanNumeral(4))
 
   }
 
