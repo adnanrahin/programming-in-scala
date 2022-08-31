0e6b501af337ffef8110f0f991a8bcd4be204726
adnanrahin <adnan.rahin13@gmail.com>
2022-08-31 00:53:32 -0400

    update

--- src/main/scala/org/functional/programming/chapter4/ScalaClass.scala
+++ src/main/scala/org/functional/programming/chapter4/ScalaClass.scala
@@ -1,5 +1,7 @@
 package org.functional.programming.chapter4
 
+import scala.collection.mutable
+
 object ScalaClass {
 
   def main(args: Array[String]): Unit = {
@@ -9,7 +11,7 @@ object ScalaClass {
 
   }
 
-  class ChecksumAccumulator {
+  class ChecksumAccumulator { // companion class of CheckSumAccumulator Object
     private var sum = 0
 
     def add(b: Byte): Unit = {
@@ -20,6 +22,22 @@ object ScalaClass {
       return ~(sum & 0xFF) + 1
     }
 
+  }
+
+  object ChecksumAccumulator { // Companion object of CheckSumAccumulator Class
+    private val cache = mutable.Map[String, Int]()
+
+    def calculate(s: String): Int =
+      if (cache.contains(s))
+        cache(s)
+      else {
+        val acc = new ChecksumAccumulator
+        for (c <- s)
+          acc.add(c.toByte)
+        val cs = acc.checksum()
+        cache += (s -> cs)
+        cs
+      }
   }
 
 }
