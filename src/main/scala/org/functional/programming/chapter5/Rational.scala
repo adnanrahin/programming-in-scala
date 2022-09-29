88adaa8ce05b6e2366acd24a608712c93f89d8e7
adnanrahin <adnan.rahin13@gmail.com>
2022-09-29 00:09:29 -0400

    update

--- src/main/scala/org/functional/programming/chapter5/Rational.scala
+++ src/main/scala/org/functional/programming/chapter5/Rational.scala
@@ -1,7 +1,17 @@
 package org.functional.programming.chapter5
 
 class Rational(n: Int, d: Int) {
-
   require(d != 0)
-  override def toString = n + "/" + d
+  val numer: Int = n
+  val denom: Int = d
+
+  override def toString = numer + "/" + denom
+
+  def this(n: Int) = this(n, 1) // auxiliary constructor
+
+  def add(that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
 }
