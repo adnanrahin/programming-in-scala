6f28ec7905e05eaaabb8c8d00ffc2c26cb570521
adnanrahin <adnan.rahin13@gmail.com>
2022-09-29 00:13:10 -0400

    auxiliary constructor

--- src/main/scala/org/functional/programming/chapter5/Rational.scala
+++ src/main/scala/org/functional/programming/chapter5/Rational.scala
@@ -2,10 +2,9 @@ package org.functional.programming.chapter5
 
 class Rational(n: Int, d: Int) {
   require(d != 0)
-  val numer: Int = n
-  val denom: Int = d
-
-  override def toString = numer + "/" + denom
+  private val g = gcd(n.abs, d.abs)
+  val numer = n / g
+  val denom = d / g
 
   def this(n: Int) = this(n, 1) // auxiliary constructor
 
@@ -14,4 +13,9 @@ class Rational(n: Int, d: Int) {
       numer * that.denom + that.numer * denom,
       denom * that.denom
     )
+
+  override def toString = numer + "/" + denom
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
 }
